ffe690924e641e83a0b2fd1620a6c3a5077c3beb
Lokendra Singh Rao <lokendras.715@gmail.com>
2024-02-27 08:12:59 +0530

    class changed to className in loading.js

--- app/(auth)/authentication/loading.js
+++ app/(auth)/authentication/loading.js
@@ -1,8 +1,8 @@
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return <div class="d-flex mt-16 justify-content-center">
-    <div class="spinner-border" role="status">
-      <span class="sr-only">Loading...</span>
+    return <div className="d-flex mt-16 justify-content-center">
+    <div className="spinner-border" role="status">
+      <span className="sr-only">Loading...</span>
     </div>
   </div>
   }
