6b7b96c3155e51ada638a6392b4f25d563fde436
Lokendra Singh Rao <lokendras.715@gmail.com>
2024-02-27 08:17:09 +0530

    removed firebase credentials

--- components/firebase.js
+++ components/firebase.js
@@ -8,12 +8,6 @@ import {getAuth} from "firebase/auth"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLCHEXOHPUu5_JPQQIZmnDDFM2DfMECQU",
-  authDomain: "react-auth-app-1.firebaseapp.com",
-  projectId: "react-auth-app-1",
-  storageBucket: "react-auth-app-1.appspot.com",
-  messagingSenderId: "351437768049",
-  appId: "1:351437768049:web:18beef9da930880ec6e2d0"
 };
 // Deployment script to be run in terminal
 // firebase deploy --only hosting:lokendra-firebase-auth-app
